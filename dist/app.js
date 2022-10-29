f3e91c17894dc6367a9cd44ea9f2458791bcad87
nsunf <jun5013@naver.com>
2022-10-29 18:15:36 +0900

    tsc

--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const index_1 = __importDefault(require("./routes/index"));
 const search_1 = __importDefault(require("./routes/search"));
 const app = (0, express_1.default)();
 app.use("/", index_1.default);
 app.use("/search", search_1.default);
-app.listen(3000, () => {
-    console.log("Express server is running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log("Express server is running on port " + process.env.PORT);
 });
