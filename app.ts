561d6f138a544df4ca329156b501bb36610c1b07
nsunf <jun5013@naver.com>
2022-10-29 16:43:02 +0900

    change port

--- app.ts
+++ app.ts
@@ -8,6 +8,6 @@ const app = express();
 app.use("/", indexRouter);
 app.use("/search", searchRouter);
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("Express server is running on port 3000");
 })
